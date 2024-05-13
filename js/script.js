2284b80833861a6291a8b029da949afbeb8ca681
denis343434 <161150159+denis343434@users.noreply.github.com>
2024-05-13 20:23:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$('.active-menu').click(function(){
+  $('.menu').toggleClass('d-n');
+});
+$('.close-menu').click(function(){
+  $('.menu').toggleClass('d-n');
+});
+$('.active-photo-slider').click(function(){
+  $('.slider-photo').toggleClass('d-n');
+});
+$('.close-photo-slider').click(function(){
+  $('.slider-photo').toggleClass('d-n');
+});
+$('.line01').click(function(){
+  $('.rotate').toggleClass('rotate1');
+});
+function toggleDiv() {
+  var div = document.getElementById('myDiv');
+  div.classList.toggle('open');
+};
+
+function toggleDiv1() {
+  var div = document.getElementById('myDiv1');
+  div.classList.toggle('open');
+};
+$('.line011').click(function(){
+  $('.rotate2').toggleClass('rotate222');
+});
+
+function toggleDiv1() {
+  var div = document.getElementById('myDiv1');
+  div.classList.toggle('open');
+};
+$('.line011').click(function(){
+  $('.rotate2').toggleClass('rotate3');
+});
+
+function toggleDiv2() {
+  var div = document.getElementById('myDiv2');
+  div.classList.toggle('open');
+};
+$('.line02').click(function(){
+  $('.rotate4').toggleClass('rotate222');
+});
+function toggleDiv3() {
+  var div = document.getElementById('myDiv3');
+  div.classList.toggle('open');
+};
+$('.line03').click(function(){
+  $('.rotate5').toggleClass('rotate222');
+});
